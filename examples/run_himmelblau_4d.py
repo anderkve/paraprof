5316360b594db0348400d01849754c70303828fc
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2025-11-05 19:40:28 +0100

    Small tweak to test script

--- examples/run_himmelblau_4d.py
+++ examples/run_himmelblau_4d.py
@@ -38,7 +38,8 @@ PROJECTIONS_TO_RUN = [
     # {'dims': [0, 1], 'grid_points': [50, 50], 'patching': False, 'lbfgsb': False, 'enable_refinement': True, 'refinement_factor': 2},
     # {'dims': [0, 1], 'grid_points': [100, 100], 'patching': False, 'lbfgsb': False, 'enable_refinement': True, 'refinement_factor': 2},
     # {'dims': [0, 1], 'grid_points': [200, 200], 'patching': False, 'lbfgsb': False, 'enable_refinement': False, 'refinement_factor': 1},
-    {'dims': [0, 2], 'grid_points': [50, 50], 'patching': False, 'lbfgsb': False, 'enable_refinement': False, 'refinement_factor': 1},
+
+    {'dims': [0, 2], 'grid_points': [50, 50], 'patching': False, 'lbfgsb': True, 'enable_refinement': True, 'refinement_factor': 4},
 ]
 
 log_likelihood, param_bounds, true_peaks = get_test_function(TEST_FUNCTION)
@@ -65,13 +66,13 @@ if myrank == 0:
         target_func=log_likelihood,
         bounds=param_bounds,
         projections=PROJECTIONS_TO_RUN,
-        pop_per_grid_point=10, # Increased for better DE
+        pop_per_grid_point=2, # Increased for better DE
         mutation_strategy='current-to-pbest/1',
         pbest_fraction=0.1,
-        n_initial_optimizations=30, # Increased
+        n_initial_optimizations=100, # Increased
         roi_threshold=3.2,
-        convergence_threshold=1e-7, # Tighter -> Looser (match serial)
-        convergence_window=5,      # Longer window -> Shorter (match serial)
+        convergence_threshold=1e-3, # Tighter -> Looser (match serial)
+        convergence_window=3,      # Longer window -> Shorter (match serial)
         neighbor_pull_probability=0.5,
         LBFGSB_ftol=1e-9,
         LBFGSB_max_iter=20,
